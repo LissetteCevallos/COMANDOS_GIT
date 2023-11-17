5243bca9c4d044c08715b3d37a4319b74ba8a2fd
LissetteCevallos <150208969+LissetteCevallos@users.noreply.github.com>
2023-11-16 22:24:06 -0500

    Add files via upload

---
+++ Deber1/src/main/java/com/mycompany/deber1/Deber1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.deber1;
+
+/**
+ *
+ * @author Lissette
+ */
+public class Deber1 {
+
+    public static void main(String[] args) {
+        Vehiculo vehiculo1 = new Vehiculo ("Toyota",2021,52,"AAC-0124");
+        vehiculo1.mover();
+        vehiculo1.trayectoria();
+    }
+}
