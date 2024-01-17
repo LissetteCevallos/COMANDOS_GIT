2d69400c9917408a60010cdccf149c325491cdfc
LissetteCevallos <150208969+LissetteCevallos@users.noreply.github.com>
2024-01-17 10:37:52 -0500

    Add files via upload

---
+++ Programa12.py
@@ -0,0 +1,15 @@
+class Persona:
+    def __init__(self,nombre,edad):
+        self.__nombre= nombre
+        self.__edad=edad
+
+    def get_nombre(self):
+        return self.__nombre
+'''
+Creamos al objecto
+'''
+dalto = Persona("Lucas",21)
+
+
+nombre = dalto.get_nombre()
+print(nombre)
